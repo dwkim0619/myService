0405ac52bf84b7157efe73aacd9c6c5bad7ae848
dwkim <dwkim0619@gmail.com>
2021-08-31 15:53:55 +0900

    .

--- src/main/java/com/myBatis/myService/MyServiceApplication.java
+++ src/main/java/com/myBatis/myService/MyServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyServiceApplication {
 
-	//
 	public static void main(String[] args) {
 		SpringApplication.run(MyServiceApplication.class, args);
 	}
